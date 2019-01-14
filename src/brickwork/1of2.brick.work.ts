3af0187386e49dfe5734d16dd211a3c7bd8f5d37
kang <dukang92@gmail.com>
2019-01-14 15:47:31 +0800

    拼法模板与示例

--- src/brickwork/1of2.brick.work.ts
+++ src/brickwork/1of2.brick.work.ts
@@ -1,13 +1,20 @@
 import {TamuBrickWorkBase} from './base/tamu.brick.work.base';
 import {AnimationBase} from '../animation/base/animation.base';
+import * as THREE from 'three';
 
 export class Oo2BrickWork implements TamuBrickWorkBase {
   private animationUtil: AnimationBase;
   constructor() {}
-  public makeObjects(isAnimate?: boolean) {
-    if (isAnimate) {
-      this.animationUtil = new AnimationBase();
-    }
-    return {obj: '', positions: [1]}
+
+  makeObject(data: any, size: THREE.Vector2): THREE.Object3D {
+    return undefined;
+  }
+
+  makeObjects(data: any, size: THREE.Vector2, isAnimate?: boolean): { objs: THREE.Object3D[]; positions: THREE.Vector3[] } {
+    return undefined;
+  }
+
+  makeVertices(data: any, size: THREE.Vector2): THREE.Vector2[] {
+    return undefined;
   }
 }
--- src/brickwork/base/tamu.brick.work.base.ts
+++ src/brickwork/base/tamu.brick.work.base.ts
@@ -1,4 +1,26 @@
 import * as THREE from 'three';
 export interface TamuBrickWorkBase {
-  makeObjects(isAnimate?: boolean): {obj: THREE.Object3D, positions: THREE.Vector3[]};
+  /**
+   * 返回可以接动画的object3D数组
+   * @param data
+   * @param {THREE.Vector2} size
+   * @param {boolean} isAnimate
+   * @returns {{objs: THREE.Object3D[]; positions: THREE.Vector3[]}}
+   */
+  makeObjects(data: any, size: THREE.Vector2, isAnimate?: boolean): {objs: THREE.Object3D[], positions: THREE.Vector3[]};
+
+  /**
+   * 返回按照大小拼好的地板object3D
+   * @param data
+   * @returns {THREE.Object3D}
+   */
+  makeObject(data: any, size: THREE.Vector2): THREE.Object3D;
+
+  /**
+   * 根据大小计算拼法
+   * @param data
+   * @param {THREE.Vector2} size
+   * @returns {THREE.Vector2[]}
+   */
+  makeVertices(data: any, size: THREE.Vector2): THREE.Vector2[];
 }
